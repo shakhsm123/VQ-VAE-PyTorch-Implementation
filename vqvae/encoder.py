84ae1bccf52ecc59ae1f7948b244563f223a42c4
Shakhnazar Zhumabekov <zshakhnazar@gmail.com>
2026-05-03 14:07:18 +0500

    Create encoder.py

---
+++ vqvae/encoder.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=2, padding=1)
+        self.norm  = nn.BatchNorm2d(output_dim)
+        self.relu  = nn.ReLU()
+        self.conv2 = nn.Conv2d(output_dim, output_dim, kernel_size=3, stride=1, padding=1)
+        self.norm2 = nn.BatchNorm2d(output_dim)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.relu2(x)
+        return x
+
+
+class EncoderModule(nn.Module):
+    def __init__(self, config: dict):
+        super().__init__()
+        channels = config["encoder_channels"]  # [128, 256, 256, 512]
+        dims = [3] + channels                  # [3, 128, 256, 256, 512]
+
+        self.encoders = nn.Sequential(
+            *[EncoderBlock(dims[i], dims[i + 1]) for i in range(len(channels))]
+        )
+        self.final = nn.Conv2d(channels[-1], config["codebook_dim"], kernel_size=1)
+
+    def forward(self, x):
+        x = self.encoders(x)
+        x = self.final(x)
+        return x
