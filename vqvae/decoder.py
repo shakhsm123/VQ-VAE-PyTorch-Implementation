5aa5a850366b0b7be803902cbab21f85ee6fb30f
Shakhnazar Zhumabekov <zshakhnazar@gmail.com>
2026-05-03 15:17:01 +0500

    Update decoder.py

--- vqvae/decoder.py
+++ vqvae/decoder.py
@@ -26,16 +26,15 @@ class DecoderBlock(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, config: dict):
         super().__init__()
-        channels = config["encoder_channels"]        # [128, 256, 256, 512]
-        dec_channels = channels[::-1]                # [512, 256, 256, 128]
-
-        self.conv_project = nn.Conv2d(config["codebook_dim"], dec_channels[0], kernel_size=1)
-
+        self.conv_project = nn.Conv2d(config["codebook_dim"], 512, kernel_size=1)
         self.decoders = nn.Sequential(
-            *[DecoderBlock(dec_channels[i], dec_channels[i + 1]) for i in range(len(dec_channels) - 1)]
+            DecoderBlock(512, 256),
+            DecoderBlock(256, 256),
+            DecoderBlock(256, 128),
+            DecoderBlock(128, 128),
         )
-        self.final_conv = nn.Conv2d(dec_channels[-1], 3, kernel_size=3, padding=1)
-        self.tanh       = nn.Tanh()
+        self.final_conv = nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = self.conv_project(x)
